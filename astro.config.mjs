132d2d08afbf9a7bffefd1ecb742801935e51182
yasuhiro.horiuchi <yasuhiro.horiuchi@grikk.page>
2025-06-14 17:55:04 +0000

    tailwindを4系に

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,23 @@
-import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import pagefind from "astro-pagefind";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-micro.vercel.app",
-  integrations: [tailwind(), sitemap(), mdx(), pagefind()],
+  integrations: [sitemap(), mdx(), pagefind()],
+
   markdown: {
     shikiConfig: {
       theme: "css-variables",
     },
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
